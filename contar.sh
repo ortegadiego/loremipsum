6349e30210572e50c1e97326aca2029ef275082d
Diego Ortega <ortegadiego@live.com.ar>
2023-04-16 19:26:37 -0300

    añadiendo archivo para contar lineas

---
+++ contar.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for i in {1..5}
+do
+    cadena=$(wc -l loremipsum-$i.txt)
+    numero=$(echo "$cadena" |  sed -r 's/^([^.]+).*$/\1/; s/^[^0-9]*([0-9]+).*$/\1/')
+    echo "loremipsum-$i.txt tiene $numero lineas."
+done
